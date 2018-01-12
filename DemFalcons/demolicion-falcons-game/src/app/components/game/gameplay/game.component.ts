37cb5df7c8218b124ad83ca7d9e2db7a1ef2c401
alpepe <sa6opp@abv.bg>
2018-01-12 17:24:09 +0200

    map1 update

--- DemFalcons/demolicion-falcons-game/src/app/components/game/gameplay/game.component.ts
+++ DemFalcons/demolicion-falcons-game/src/app/components/game/gameplay/game.component.ts
@@ -21,7 +21,7 @@ export class GameComponent implements OnInit, OnDestroy {
     private playerService: PlayerService,
     private diceService: DiceService
   ) {
-    this.paper = Raphael('map', 100, 1300, 2000)
+    this.paper = Raphael('map', 100, 1340, 2000)
     this.setVisibleProperty();
   }
 
