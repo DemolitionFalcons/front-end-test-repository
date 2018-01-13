7d0eecde57747bce963fafe24e3470aa26b82eae
alpepe <sa6opp@abv.bg>
2018-01-13 19:39:33 +0200

    throwing a dice, end map responsive

--- DemFalcons/demolicion-falcons-game/src/app/components/game/gameplay/game.component.ts
+++ DemFalcons/demolicion-falcons-game/src/app/components/game/gameplay/game.component.ts
@@ -3,6 +3,7 @@ import * as Raphael from 'raphael/raphael.js';
 import { MapService } from '../../../core/services/game-services/map.service';
 import { PlayerService } from '../../../core/services/game-services/player.service';
 import { DiceService } from '../../../core/services/game-services/dice.service';
+import { setTimeout } from 'timers';
 
 
 
@@ -16,31 +17,41 @@ export class GameComponent implements OnInit, OnDestroy {
 
   public paper;
   public isDiceRolled = false;
+
+  public diseStart = false;
+  public diseThrown = false;
+  public diseImage = true;
+  public disethrownHide = true;
+  public hide1 = true;
+  public dise: number;
+
   constructor(
     private mapService: MapService,
     private playerService: PlayerService,
-    private diceService: DiceService
+    // private diceService: DiceService
   ) {
 
-    let width = window.document.body.clientWidth
+    // window.addEventListener('resize', this.windowResizing);
+    let width = window.document.body.clientWidth;
 
-    this.paper = Raphael('map', 50, '100%', '250%')
-    this.paper.setViewBox(0, 0, width + 200, 1700, true);
-    this.paper.canvas.setAttribute('preserveAspectRatio', 'none');
-    this.paper.image("../../../../assets/img/map1background.jpg", 0, 20, '70%', '100%');
+    this.paper = Raphael('map', 50, '100%', '250%');
+    this.paper.setViewBox(0, 0, width + 200, 1900, true);
+    this.paper.canvas.setAttribute('preserveAspectRatio', 'xMinYMin');
+    this.paper.image("../../../../assets/img/map1background.jpg", 0, 20, 1100, 1900);
     this.setVisibleProperty();
+
   }
 
 
   ngOnInit() {
     this.mapService.drawMap(this.paper);
     this.playerService.drawPlayers(this.paper);
-    this.diceService.drawDice(this.paper);
+    // this.diceService.drawDice(this.paper);
   }
 
   ngOnDestroy() {
 
-  this.paper.remove();
+    this.paper.remove();
   }
 
   private setVisibleProperty() {
@@ -50,8 +61,26 @@ export class GameComponent implements OnInit, OnDestroy {
   }
 
   rollDice() {
-    this.diceService.rollDice();
+    // this.diceService.rollDice();
     console.log('in roll dice');
+    this.dise = Math.floor(Math.random() * 6) + 1;
+    this.diseStart = true;
+    this.hide1 = true;
+    setTimeout(() => {
+      this.diseStart = false;
+      this.diseThrown = true;
+      this.disethrownHide = false;
+    }, 1000, );
+    setTimeout(() => {
+      this.disethrownHide = true;
+      this.hide1 = false;
+      this.playerService.movePlayer(this.dise);
+    }, 2000, );
+
+
+
+
+
     // if (this.isDiceRolled === false) {
     //   this.isDiceRolled = true;
     //   //request for dice?
@@ -62,6 +91,32 @@ export class GameComponent implements OnInit, OnDestroy {
     // }
   }
 
-}
+  windowResizing() {
+
+    //   let width = window.document.body.clientWidth;
+    //   if (width <= 500) {
+    //     this.paper ? this.paper.remove() : console.log('none');
+
+    //     console.log('under500px');
+    //     this.paper = Raphael('map', 50, '100%', '250%');
+    //     this.paper.setViewBox(0, 0, width + 200, 1700, true);
+    //     this.paper.canvas.setAttribute('preserveAspectRatio', 'xMinYMin');
+    //     this.paper ? this.paper.image("../../../../assets/img/map1background.jpg", 0, 20, '70%', '110%') : console.log('none');
+    //     Raphael.el.isVisible = function () {
+    //       return (this.node.style.display !== "none");
+    //     }
+    //     this.mapService.drawMap(this.paper);
+    //     this.playerService.drawPlayers(this.paper);
+    //     this.diceService.drawDice(this.paper);
+    //   } else{
+    //     this.paper = Raphael('map', 50, '100%', '250%');
+    //     this.paper.setViewBox(0, 0, width + 200, 1700, true);
+    //     this.paper.canvas.setAttribute('preserveAspectRatio', 'xMinYMin');
+    //     this.paper.image("../../../../assets/img/map1background.jpg", 0, 20, '70%', '110%');
+    //     this.setVisibleProperty();
+    //   }
+    // }
+
+  }
 
 
